0603722320c56841c192c4a08943991c7fcaee26
Mezen jebali <jebali.mazen@gmail.com>
2024-09-07 18:31:28 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ app.get('/', function (req, res) {
 });
 // create entity
 const sample = DB.model("shortenedUrl", new DB.Schema({
-  originalUrl: {
+  original_url: {
     type: String,
     required: true,
     unique: true
   },
-  shortUrl: {
+  short_url: {
     type: String,
     required: true,
     unique: true
@@ -62,10 +62,10 @@ const generateShorturl = async (req, res, next) => {
 
 const saveShorturl = async (req, res, next) => {
   if (req.shorturl) {
-    const search = await sample.findOne({ originalUrl: req.original });
-    
+    const search = await sample.findOne({ original_url: req.original },"-_id original_url short_url");
+
     if (!search) {
-      req.data = new sample({ originalUrl: req.original, shortUrl: req.shorturl });
+      req.data = new sample({ original_url: req.original, short_url: req.shorturl });
 
       req.data.save().then((r) => {
         console.log("link saved!");
@@ -81,7 +81,7 @@ const saveShorturl = async (req, res, next) => {
 }
 
 const loadShorturl = async (req, res, next) => {
-  req.data = await sample.findOne({ shortUrl: req.params.urlcode }).exec();
+  req.data = await sample.findOne({ short_url: req.params.short_url },"-_id original_url short_url").exec();
   next();
 }
 
@@ -97,8 +97,8 @@ app.post("/api/shorturl", parser.urlencoded({ extended: true }),
       res.json(req.data);
     }
   });
-app.get("/api/shorturl/:urlcode", loadShorturl, (req, res) => {
-  res.redirect(req.data.originalUrl);
+app.get("/api/shorturl/:short_url", loadShorturl, (req, res) => {
+  res.redirect(req.data.original_url);
 })
 
 // Your first API endpoint
